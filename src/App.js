b07e32c897c3c62307afe19e5a360435911a86a2
Matteo Carpi <matteocarpi94@gmail.com>
2020-04-21 15:20:18 +0200

    fix: home is dashboard

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Home from './pages/Home';
+import Home from './pages/Dashboard';
 import Login from './pages/Login';
 import Signup from './pages/SignUp';
+import NewBancale from './pages/NewBancale';
 import { AuthProvider } from './components/Auth';
 import PrivateRoute from './components/PrivateRoute';
 
@@ -13,6 +14,7 @@ function App() {
       <Router>
         <div>
           <PrivateRoute exact path="/" component={Home} />
+          <PrivateRoute exact path="/new-bancale" component={NewBancale} />
           <Route exact path="/login" component={Login} />
           <Route exact path="/signup" component={Signup} />
         </div>
---
+++ src/pages/Dashboard.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Layout from './Layout';
+
+const Dashboard = () => {
+  return (
+    <Layout>
+      <div>
+        <h1>Dashboard</h1>
+        <Link to="/new-bancale">Add Bancale</Link>
+      </div>
+    </Layout>
+  );
+};
+
+export default Dashboard;
