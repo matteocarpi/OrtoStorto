81569883b27f7ddb4ca17f8a133cfe3426ac05b0
Matteo Carpi <matteocarpi94@gmail.com>
2020-04-21 17:16:41 +0200

    chore: installed reactotron

--- src/ReactotronConfig.js
+++ src/ReactotronConfig.js
@@ -1,5 +1,13 @@
+<<<<<<< HEAD
 import Reactotron from 'reactotron-react-js';
 
 Reactotron
   .configure() // we can use plugins here -- more on this later
   .connect(); // let's connect!
+=======
+import Reactotron from 'reactotron-react-js'
+
+Reactotron
+  .configure() // we can use plugins here -- more on this later
+  .connect() // let's connect!
+>>>>>>> chore: installed reactotron
