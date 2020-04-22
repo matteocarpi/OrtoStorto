c60788192e7138820c4449084966e2225a3cc697
Matteo Carpi <matteocarpi94@gmail.com>
2020-04-22 18:11:44 +0200

    feat: a bit of style for bancali

--- src/pages/Bancali.js
+++ src/pages/Bancali.js
@@ -3,6 +3,7 @@ import Layout from '../components/Layout';
 import { Link } from 'react-router-dom';
 import { useFind } from 'react-pouchdb';
 import Reactotron from 'reactotron-react-js';
+import styles from '../styles/BancaliList.module.scss';
 
 const Bancali = () => {
   const bancali = useFind({
@@ -20,16 +21,18 @@ const Bancali = () => {
   // );
   return (
     <Layout>
-      <h1>Bancali</h1>
-      {bancali.map((bancale, i) => {
-        return (
-          <>
-            <h2 key={i}>{bancale.number}</h2>
-            <p>{bancale.family}</p>
-          </>
-        );
-      })}
-      <Link to="/new-bancale">Crea Nuovo Bancale</Link>
+      <h1 className={styles.title}>Bancali</h1>
+      <section className={styles.bancali}>
+        {bancali.map((bancale, i) => {
+          return (
+            <article key={i} className={styles.bancale}>
+              <h2>{bancale.number}</h2>
+              <p>{bancale.family}</p>
+            </article>
+          );
+        })}
+      </section>
+      <Link className={styles.createNew} to="/new-bancale">Crea Nuovo Bancale</Link>
     </Layout>
   );
 };
