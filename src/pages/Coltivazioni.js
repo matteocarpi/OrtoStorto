33334abc7822675d0c5b4f97478704c0fd9bcb35
Matteo Carpi <matteocarpi94@gmail.com>
2020-04-25 18:27:42 +0200

    feat: coltivazioni page

---
+++ src/pages/Coltivazioni.js
@@ -0,0 +1,13 @@
+import React, { useEffect } from 'react';
+
+const Coltivazioni = () => {
+  
+  return (
+    <div>
+      <h1>Coltivazioni</h1>
+
+    </div>
+  );
+};
+
+export default Coltivazioni;
