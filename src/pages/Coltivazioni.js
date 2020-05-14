4feab1d7598ac32c36f86f8a5a50dc98b0b5b286
Matteo Carpi <matteocarpi94@gmail.com>
2020-05-14 17:25:17 +0200

    feat: simple table in coltivazioni

--- src/pages/Coltivazioni.js
+++ src/pages/Coltivazioni.js
@@ -3,9 +3,19 @@ import { Link, useHistory } from 'react-router-dom';
 import styles from '../styles/Coltivazioni.module.scss';
 import { useDB } from '../services/pouchDB';
 import Layout from '../components/Layout';
+import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+  table: {
+    minWidth: 650,
+  },
+});
 
 const Coltivazioni = () => {
+  const classes = useStyles();
   const db = useDB();
+
   const history = useHistory();
 
   const [coltivazioni, setColtivazioni] = useState();
@@ -31,11 +41,35 @@ const Coltivazioni = () => {
   return (
     <Layout>
 
-      <div className={styles.wrap}>
+      <Paper className={styles.wrap}>
         <Link className={styles.createNew} to="/nuova-coltivazione">Crea Nuova Coltivazione</Link>
 
         <h1>Coltivazioni</h1>
-        <table>
+
+        <TableContainer component={Paper}>
+          <Table className={classes.table} aria-label="Coltivazioni">
+            <TableHead>
+              <TableRow>
+                <TableCell>Varietà</TableCell>
+                <TableCell>Famiglia</TableCell>
+                <TableCell>Data di Semina</TableCell>
+              </TableRow>
+            </TableHead>
+
+            {coltivazioni && coltivazioni.map((coltivazione, index) => {
+              return (
+                <TableRow key={index}>
+                  <TableCell>{coltivazione.name}</TableCell>
+                  <TableCell>{coltivazione.family}</TableCell>
+                  <TableCell>{coltivazione.date}</TableCell>
+                </TableRow>
+              );
+            },
+            )
+            }
+          </Table>
+        </TableContainer>
+        {/* <table>
           <tbody>
             <th>Name</th>
             <th>Family</th>
@@ -52,8 +86,8 @@ const Coltivazioni = () => {
             })}
 
           </tbody>
-        </table>
-      </div>
+        </table> */}
+      </Paper>
     
     </Layout>
   );
